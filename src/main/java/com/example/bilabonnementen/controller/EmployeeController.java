34e0263296a93773decca5b8f140f4e1e31f207f
Yasi1095 <yasi1095@stud.kea.dk>
2023-05-09 16:24:38 +0200

    hej

--- src/main/java/com/example/bilabonnementen/controller/EmployeeController.java
+++ src/main/java/com/example/bilabonnementen/controller/EmployeeController.java
@@ -17,16 +17,9 @@ public class EmployeeController {
     @GetMapping("/personale")
     public String getAllEmployees(Model model){
         List <Employee> employees = employeeService.fetchAllEmployees();
-        System.out.println(employees.size());
         model.addAttribute("employees", employees);
         return "personale";
     }
-    @GetMapping("/test")
-    public String testAllEmployees(Model model){
-        List <Employee> employees = employeeService.fetchAllEmployees();
-        System.out.println(employees.size());
-        model.addAttribute("employees", employees);
-        return "test";
-    }
+
 
 }
