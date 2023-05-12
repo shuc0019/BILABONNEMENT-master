1ae8ae941af27121e83eaa3d11eef0977a7e56be
suusmita <susmidey2001@gmail.com>
2023-05-12 12:21:50 +0200

    update car html

--- src/main/java/com/example/bilabonnementen/controller/CarController.java
+++ src/main/java/com/example/bilabonnementen/controller/CarController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -53,12 +54,15 @@ public class CarController {
         }
     }
 
-    @GetMapping("/opdaterBil")
+    @GetMapping("/opdaterBilen")
     public String updateCar() {
         return "opdaterBil";
     }
 
-
-
+    @PostMapping("/opdaterBiler")
+    public String updateCarToList(Car car) {
+        carService.updateCar(car);
+        return "redirect:/seallebiler";
+    }
 
     }
