ceaab2f84be4eafe9d56c0459f3d828cf49a0094
shuc0019 <shuc0019@stud.kea.dk>
2023-05-18 15:27:03 +0200

    hh

--- src/main/java/com/example/bilabonnementen/service/DamageService.java
+++ src/main/java/com/example/bilabonnementen/service/DamageService.java
@@ -3,6 +3,7 @@ package com.example.bilabonnementen.service;
 import com.example.bilabonnementen.model.Damage_category;
 import com.example.bilabonnementen.model.Specific_damage;
 import com.example.bilabonnementen.repository.DamageRepo;
+import jdk.jfr.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,9 @@ public class DamageService {
 
     public List<Damage_category> fetchAllDamageCategories(){
         return damageRepo.fetchAll();
+    }
+
+    public Double getSpecificDamagePrice(int category_id) {
+        return damageRepo.findSpecificDamagePrice(category_id);
     }
 }
